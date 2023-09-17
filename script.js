646c7278d00824e3dbbcea44931454bd425642a3
ARTWORKHEX <112499761+artworkhex@users.noreply.github.com>
2023-09-17 22:11:24 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function updatemenu() {
+    if (document.getElementById('responsive-menu').checked == true) {
+        document.getElementById('menu').style.borderBottomRightRadius = '0';
+        document.getElementById('menu').style.borderBottomLeftRadius = '0';
+    } else {
+        document.getElementById('menu').style.borderRadius = '10px';
+    }
+}
+
+
+
+// auto hide menu navbar
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("manu").style.top = "0";
+    } else {
+        document.getElementById("manu").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+}
